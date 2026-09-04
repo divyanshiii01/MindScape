5e398f055cc565f5b5d243ec0b3e78c0e28dd5db
divyanshiii01 <dc5626181@gmail.com>
2026-09-04 18:15:25 +0530

    Use environment variable for API URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function MindScapeLogo() {
   return (
